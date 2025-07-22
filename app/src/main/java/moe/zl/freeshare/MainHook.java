14485ce2d940184d1c47b89a123cbc21d1336b4f
ZL114514 <ouyangzihao009@gmail.com>
2025-07-22 16:47:12 +0800

    init

---
+++ app/src/main/java/moe/zl/freeshare/MainHook.java
@@ -0,0 +1,200 @@
+package moe.zl.freeshare;
+
+import android.app.Activity;
+import android.app.ActivityOptions;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.PointF;
+import android.graphics.RectF;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.view.View;
+import android.graphics.Rect;
+import android.view.WindowInsets;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+import java.lang.Throwable;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.List;
+
+public class MainHook implements IXposedHookLoadPackage {
+
+  private String TAG = "Free Launcher";
+
+  @Override
+  public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+
+    XposedBridge.log(TAG + ": Found target package: " + lpparam.packageName);
+    try {
+      XposedHelpers.findAndHookMethod(
+          "com.android.quickstep.AbsSwipeUpHandler", // 目标类的完整名称
+          lpparam.classLoader, // 目标应用的类加载器
+          "updateSysUiFlags", // 目标方法的名称
+          float.class, // 目标方法的参数类型列表 (这里只有一个 float)
+          new XC_MethodHook() { // Hook 逻辑回调
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+              float progress = (float) param.args[0];
+              Object mRecentsView = XposedHelpers.getObjectField(param.thisObject, "mRecentsView");
+              if (progress > 2) {
+                if (mRecentsView != null) {
+                  int[] position = new int[2];
+                  Object mTaskView = XposedHelpers.callMethod(mRecentsView, "getRunningTaskView");
+                  Object mTaskContainer =
+                      XposedHelpers.callMethod(
+                          XposedHelpers.callMethod(mTaskView, "getTaskContainers"), "get", 0);
+                  XposedHelpers.callMethod(mTaskContainer, "setOverlayEnabled", true);
+                  Object mSnapshotView =
+                      XposedHelpers.callMethod(mTaskContainer, "getSnapshotView");
+                  // XposedHelpers.callMethod(mSnapshotView, "setBackgroundColor",
+                  // 0xff1f1e33);
+                  XposedHelpers.callMethod(mSnapshotView, "getLocationOnScreen", position);
+                  int width =
+                      (int)
+                          ((int) XposedHelpers.callMethod(mSnapshotView, "getWidth")
+                              * (float) XposedHelpers.callMethod(mTaskView, "getScaleX"));
+                  int height =
+                      (int)
+                          ((int) XposedHelpers.callMethod(mSnapshotView, "getHeight")
+                              * (float) XposedHelpers.callMethod(mTaskView, "getScaleY"));
+                  Rect taskBounds =
+                      new Rect(position[0], position[1], position[0] + width, position[1] + height);
+                  // XposedBridge.log(TAG + ": " + taskBounds.toString());
+
+                }
+              }
+              /* String logMessage = "updateDisplacement called with value: " + progress;
+              XposedBridge.log(TAG + ": " + logMessage);*/
+            }
+          });
+      XposedHelpers.findAndHookMethod(
+          "com.android.quickstep.AbsSwipeUpHandler", // 目标类的完整名称
+          lpparam.classLoader, // 目标应用的类加载器
+          "onCalculateEndTarget", // 目标方法的名称
+          new XC_MethodHook() { // Hook 逻辑回调
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+              float progress =
+                  (float)
+                      XposedHelpers.getObjectField(
+                          XposedHelpers.getObjectField(param.thisObject, "mCurrentShift"), "value");
+              String logMessage = "end gesture: " + progress;
+              XposedBridge.log(TAG + ": " + logMessage);
+              Object mRecentsView = XposedHelpers.getObjectField(param.thisObject, "mRecentsView");
+              if (progress > 3) {
+                if (mRecentsView != null) {
+                  int[] position = new int[2];
+                  Object HOME_TARGET =
+                      XposedHelpers.findClass(
+                              "com.android.quickstep.GestureState.GestureEndTarget",
+                              lpparam.classLoader)
+                          .getEnumConstants()[0];
+                  Object mTaskView = XposedHelpers.callMethod(mRecentsView, "getRunningTaskView");
+                  Object mTaskContainer =
+                      XposedHelpers.callMethod(
+                          XposedHelpers.callMethod(mTaskView, "getTaskContainers"), "get", 0);
+                  XposedHelpers.callMethod(mTaskContainer, "setOverlayEnabled", true);
+                  Object mSnapshotView =
+                      XposedHelpers.callMethod(mTaskContainer, "getSnapshotView");
+                  // XposedHelpers.callMethod(mSnapshotView, "setBackgroundColor",
+                  // 0xff1f1e33);
+                  XposedHelpers.callMethod(mSnapshotView, "getLocationOnScreen", position);
+                  int width =
+                      (int)
+                          ((int) XposedHelpers.callMethod(mSnapshotView, "getWidth")
+                              * (float) XposedHelpers.callMethod(mTaskView, "getScaleX"));
+                  int height =
+                      (int)
+                          ((int) XposedHelpers.callMethod(mSnapshotView, "getHeight")
+                              * (float) XposedHelpers.callMethod(mTaskView, "getScaleY"));
+                  Rect taskBounds =
+                      new Rect(position[0], position[1], position[0] + width, position[1] + height);
+                  // XposedBridge.log(TAG + ": " + taskBounds.toString());
+
+                  XposedHelpers.callMethod(
+                      XposedHelpers.getObjectField(param.thisObject, "mGestureState"),
+                      "setEndTarget",
+                      HOME_TARGET);
+
+                  /*XposedHelpers.callMethod(
+                  XposedHelpers.newInstance(
+                      XposedHelpers.findClass(
+                          "com.android.systemui.shared.system.ActivityManagerWrapper",
+                          lpparam.classLoader)),
+                  "startActivityFromRecents",
+                  XposedHelpers.getObjectField(
+                      XposedHelpers.getObjectField(mTaskContainer, "task"), "key"),
+                  getOpt(taskBounds));*/
+                  String FREEFORM_PACKAGE = "com.libremobileos.freeform";
+                  String FREEFORM_INTENT = "com.libremobileos.freeform.START_FREEFORM";
+
+                  Object task = XposedHelpers.getObjectField(mTaskContainer, "task");
+                  final Intent intent = new Intent(FREEFORM_INTENT).setPackage(FREEFORM_PACKAGE);
+                  XposedHelpers.callMethod(
+                      intent,
+                      "putExtra",
+                      "packageName",
+                      XposedHelpers.callMethod(
+                          XposedHelpers.getObjectField(task, "key"), "getPackageName"));
+                  XposedHelpers.callMethod(
+                      intent,
+                      "putExtra",
+                      "activityName",
+                      XposedHelpers.callMethod(
+                          XposedHelpers.callMethod(task, "getTopComponent"), "getClassName"));
+                  XposedHelpers.callMethod(
+                      intent,
+                      "putExtra",
+                      "userId",
+                      XposedHelpers.getObjectField(
+                          XposedHelpers.getObjectField(task, "key"), "userId"));
+                  XposedHelpers.callMethod(
+                      intent,
+                      "putExtra",
+                      "taskId",
+                      XposedHelpers.getObjectField(
+                          XposedHelpers.getObjectField(task, "key"), "id"));
+
+                  XposedHelpers.callMethod(
+                      XposedHelpers.getObjectField(param.thisObject, "mContext"),
+                      "sendBroadcast",
+                      intent);
+                }
+              }
+            }
+          });
+    } catch (XposedHelpers.ClassNotFoundError e) {
+      XposedBridge.log(TAG + ": Class not found ");
+      XposedBridge.log(e);
+    } catch (NoSuchMethodError e) {
+      XposedBridge.log(TAG + ": Method not found ");
+      XposedBridge.log(e);
+    } catch (Throwable t) {
+      XposedBridge.log(TAG + ": An unexpected error occurred during hooking.");
+      XposedBridge.log(t);
+    }
+  }
+
+  ActivityOptions getOpt(Rect r) {
+    ActivityOptions opt = ActivityOptions.makeBasic();
+
+    XposedHelpers.callMethod(opt, "setLaunchWindowingMode", 5);
+    XposedHelpers.callMethod(opt, "setTaskAlwaysOnTop", true);
+    XposedHelpers.callMethod(opt, "setTaskOverlay", true, true);
+    XposedHelpers.callMethod(opt, "setApplyMultipleTaskFlagForShortcut", true);
+    XposedHelpers.callMethod(opt, "setApplyActivityFlagsForBubbles", true);
+    XposedHelpers.callMethod(opt, "setLaunchedFromBubble", true);
+    /* final View decorView = container.getWindow().getDecorView();
+    final WindowInsets insets = decorView.getRootWindowInsets();
+    r.offsetTo(insets.getSystemWindowInsetLeft() + 50, insets.getSystemWindowInsetTop() + 50);*/
+    opt.setLaunchBounds(r);
+
+    return opt;
+  }
+}
